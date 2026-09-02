4e78ddc400dc62ecd53e5a5a8e444e09b5cace30
Nicolas Meira <nicomeira05@gmail.com>
2026-09-02 15:43:28 -0300

    Entrega 2 completa

--- entrega-2-meira-nicolas/abstract_projectile.gd
+++ entrega-2-meira-nicolas/abstract_projectile.gd
@@ -22,3 +22,7 @@ func _physics_process(delta):
 
 func _on_timer_timeout() -> void:
 	emit_signal("delete_requested", self)
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	queue_free() #borra el nodo cuando cruce la pantalla del juego
--- entrega-2-meira-nicolas/main.gd
+++ entrega-2-meira-nicolas/main.gd
@@ -1,10 +1,29 @@
 extends Node2D
 
+var turret_scene: PackedScene = preload("res://entities/enemies/turret.tscn")
 
 
 func _ready() -> void:
 	$Player.set_projectile_container(self)
-	$Turret.set_values($Player, self)
+	#$Turret.set_values($Player, self) esto era para cuando solo tenia 1 torreta fija
+	randomize()
+	for i in range(3):
+		spawn_turret()
+		
+func spawn_turret():
+	var turret_instance = turret_scene.instantiate()
+	var screen_width = get_viewport_rect().size.x #limite de la pantalla seria el size
+	#posicion x aleatoria, dentro de la imagen del juego
+	var random_x = randf_range(50, screen_width - 50)
+	
+	#posicion Y aleatoria, por arriba del jugador
+	var random_y = randf_range(50, $Player.global_position.y - 50)
+	
+	#asignamos las posiciones aleatorias a la torreta
+	turret_instance.global_position = Vector2(random_x, random_y)
+	
+	add_child(turret_instance)
+	turret_instance.set_values($Player, self)		
 	
 	
  
